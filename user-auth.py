1a492501f29f18015af6de4b41f34d264d42f710
Joel Gilger <joelgilger@gmail.com>
2024-05-04 23:09:00 -0700

    update with signin route

--- user-auth.py
+++ user-auth.py
@@ -7,8 +7,8 @@ CORS(app)
 app.config['SECRET_KEY'] = 'my_key'
 
 users = {
-    'user1': {'password': 'password1'},
-
+    'user1': {'password': 'password1', 'email': 'j@g.com'},
+    ''     : {'password': '1234', 'email': 'j@g.com'}
     }
 
 
@@ -23,7 +23,18 @@ def login():
     else:
         return jsonify({'message': 'Invalid username or password'}), 401
 
-
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    email = data.get('email')
+    if username in users:
+        return jsonify({'message': 'Username already in use'})
+    if username not in users:
+        users[username] = {'password': password, 'email': email}
+        print(users)
+        return jsonify({'message': 'Success'})
 
 if __name__ == '__main__':
     app.run(debug=True)
